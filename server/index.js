3deadeb5ae5b513c698539632c8f71210b7b9ec2
mark-p7 <markdeguzman1142@gmail.com>
2024-01-20 14:30:17 -0800

    Added MongoDB to server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,17 @@
 const express = require('express');
 const app = express();
 const port = 8888;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/local";
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+const db = mongoose.connection;
 
 // Start server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
