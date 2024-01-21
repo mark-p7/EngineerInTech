50396069fed35548686de64c376ea830adbc0379
mark-p7 <markdeguzman1142@gmail.com>
2024-01-21 01:00:03 -0800

    Modified chat schema to reflect new backend changes to messages

--- server/models/chat.js
+++ server/models/chat.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 
+// Message list type is an array of objects with the following properties:
+// - ownerID: the id of the user who sent the message
+// - text: the text of the message
+// - createdAt: the date the message was created
+
 const Chat = new mongoose.Schema(
     {
         messageList: {
-            type: [String],
+            type: [Object],
             default: [],
         },
         userIds: {
