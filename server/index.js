c6b797501f44370dfccbcc97aea0bb377390a6e4
mark-p7 <markdeguzman1142@gmail.com>
2024-01-20 21:44:57 -0800

    Added modify routes to server

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const authRouter = require("./authRoutes");
 const readRouter = require("./readRoutes");
+const modifyRouter = require("./modifyRoutes");
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/local";
 
@@ -19,6 +20,7 @@ const db = mongoose.connection;
 app.use(express.json());
 app.use("/api", authRouter);
 app.use("/api", readRouter);
+app.use("/api", modifyRouter);
 
 // Start server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
